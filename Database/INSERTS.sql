a6e46ae5ba33e16ac36e8bc16d2f27b39ea60026
AtlasRW <atlasrw@icloud.com>
2021-12-06 03:29:43 +0000

    inserts

---
+++ Database/INSERTS.sql
@@ -0,0 +1,64 @@
+USE [MINT_DASHBOARD_BUILDER]
+
+INSERT INTO [CATEGORY] (title) VALUES ('application');
+INSERT INTO [CATEGORY] (title) VALUES ('attitude');
+INSERT INTO [CATEGORY] (title) VALUES ('structure');
+INSERT INTO [CATEGORY] (title) VALUES ('access');
+INSERT INTO [CATEGORY] (title) VALUES ('hardware');
+INSERT INTO [CATEGORY] (title) VALUES ('Synergistic');
+INSERT INTO [CATEGORY] (title) VALUES ('real-time');
+INSERT INTO [CATEGORY] (title) VALUES ('24 hour');
+INSERT INTO [CATEGORY] (title) VALUES ('intranet');
+INSERT INTO [CATEGORY] (title) VALUES ('5th generation');
+
+INSERT INTO [ACTION] (title, link) VALUES ('knowledge user', '/eu/nibh/quisque/id/justo/sit.jsp?tristique=ut');
+INSERT INTO [ACTION] (title, link) VALUES ('time-frame', '/nascetur/ridiculus/mus/vivamus/vestibulum/sagittis.aspx?donec=potenti');
+INSERT INTO [ACTION] (title, link) VALUES ('Right-sized', '/turpis/elementum/ligula/vehicula/consequat/morbi.png?placerat=justo');
+INSERT INTO [ACTION] (title, link) VALUES ('throughput', '/pede.js?quis=ultrices&tortor=posuere');
+INSERT INTO [ACTION] (title, link) VALUES ('productivity', '/aliquet/at/feugiat/non.aspx?lacinia=et');
+INSERT INTO [ACTION] (title, link) VALUES ('eco-centric', '/dapibus.xml?turpis=sit');
+INSERT INTO [ACTION] (title, link) VALUES ('optimal', '/sed/nisl/nunc/rhoncus/dui/vel/sem.jpg?at=mauris&vulputate=morbi');
+INSERT INTO [ACTION] (title, link) VALUES ('alliance', '/nunc/proin/at.html?odio=duis&consequat=consequat');
+INSERT INTO [ACTION] (title, link) VALUES ('neural-net', '/rhoncus/dui/vel.js?convallis=vitae');
+INSERT INTO [ACTION] (title, link) VALUES ('parallelism', '/suspendisse/accumsan/tortor/quis/turpis.jsp?libero=nulla');
+
+INSERT INTO [MESSAGE] (title, is_draft, header, body, background, start_date, end_date) VALUES ('458guo594', 0, 'justo sollicitudin ut suscipit a', 'lacinia aenean sit amet justo morbi ut odio', 'ABCDEF', '2020-09-01 23:21:35', '2025-11-26 11:16:47');
+INSERT INTO [MESSAGE] (title, is_draft, header, body, background, start_date, end_date) VALUES ('398usq933', 0, 'vestibulum sit amet cursus', 'maecenas tristique est et tempus', 'ABCDEF', '2021-06-01 00:50:16', '2025-08-27 20:47:15');
+INSERT INTO [MESSAGE] (title, is_draft, header, body, background, start_date, end_date) VALUES ('593utt116', 0, 'ipsum primis in faucibus', 'ipsum ac tellus semper interdum mauris ullamcorper purus sit amet nulla quisque arcu', 'ABCDEF', '2021-01-26 09:56:45', '2024-02-08 23:46:58');
+INSERT INTO [MESSAGE] (title, is_draft, header, body, background, start_date, end_date) VALUES ('072bbc686', 0, 'eget', 'in faucibus orci luctus et ultrices posuere cubilia curae nulla dapibus dolor vel est donec odio justo sollicitudin ut', 'ABCDEF', '2021-07-05 20:41:25', '2021-08-13 20:08:02');
+INSERT INTO [MESSAGE] (title, is_draft, header, body, background, start_date, end_date) VALUES ('709rvo136', 0, 'risus', 'metus aenean fermentum donec ut mauris eget', 'ABCDEF', '2021-06-17 17:29:11', '2021-10-16 15:37:16');
+INSERT INTO [MESSAGE] (title, is_draft, header, body, background, start_date, end_date) VALUES ('324gse225', 0, 'nibh quisque id justo sit amet sapien', 'lobortis est phasellus sit amet erat nulla tempus vivamus in felis eu sapien', 'ABCDEF', '2021-01-08 19:53:54', '2021-02-16 07:30:23');
+INSERT INTO [MESSAGE] (title, is_draft, header, body, background, start_date, end_date) VALUES ('474yvp112', 0, null, null, 'ABCDEF', '2021-09-01 11:33:07', '2021-05-19 20:30:13');
+INSERT INTO [MESSAGE] (title, is_draft, header, body, background, start_date, end_date) VALUES ('278mce350', 0, 'ante nulla justo aliquam quis turpis eget elit', 'eu sapien cursus vestibulum proin eu mi nulla ac enim in tempor turpis nec euismod scelerisque quam', 'ABCDEF', '2021-03-25 19:36:29', '2021-11-01 11:55:18');
+INSERT INTO [MESSAGE] (title, is_draft, header, body, background, start_date, end_date) VALUES ('612veq188', 1, null, null, 'ABCDEF', '2021-09-20 13:46:06', '2021-02-15 16:26:00');
+INSERT INTO [MESSAGE] (title, is_draft, header, body, background, start_date, end_date) VALUES ('156dnz678', 0, 'viverra eget congue eget semper rutrum nulla', 'eleifend donec ut dolor morbi', 'ABCDEF', '2021-07-23 22:55:43', '2021-07-16 17:06:59');
+INSERT INTO [MESSAGE] (title, is_draft, header, body, background, start_date, end_date) VALUES ('228gpa799', 1, 'cursus urna ut tellus nulla ut erat id', 'neque duis bibendum morbi non quam nec dui', 'ABCDEF', '2021-06-11 00:02:03', '2021-01-20 21:26:43');
+INSERT INTO [MESSAGE] (title, is_draft, header, body, background, start_date, end_date) VALUES ('525kfb708', 0, 'eget', 'habitasse platea dictumst morbi vestibulum velit id pretium iaculis diam erat fermentum justo nec condimentum neque sapien', 'ABCDEF', '2021-07-14 05:27:53', '2020-11-21 13:16:15');
+INSERT INTO [MESSAGE] (title, is_draft, header, body, background, start_date, end_date) VALUES ('843ypm585', 0, 'quis turpis eget elit sodales scelerisque mauris sit', 'justo nec condimentum neque sapien placerat ante nulla justo aliquam quis turpis eget elit sodales scelerisque mauris sit', 'ABCDEF', '2020-12-27 14:07:49', '2021-03-14 07:15:45');
+INSERT INTO [MESSAGE] (title, is_draft, header, body, background, start_date, end_date) VALUES ('984msf395', 1, null, null, 'ABCDEF', '2021-02-25 23:00:32', '2020-12-30 07:35:43');
+INSERT INTO [MESSAGE] (title, is_draft, header, body, background, start_date, end_date) VALUES ('304nwq085', 1, 'morbi a ipsum integer a nibh in quis justo', 'fusce consequat nulla nisl nunc', 'ABCDEF', '2021-05-20 20:41:21', '2021-01-03 02:59:34');
+INSERT INTO [MESSAGE] (title, is_draft, header, body, background, start_date, end_date) VALUES ('601prp602', 1, 'montes nascetur ridiculus mus etiam vel augue', 'condimentum curabitur in libero ut massa volutpat convallis morbi odio odio elementum eu interdum eu', 'ABCDEF', '2021-03-29 20:28:11', '2021-10-12 02:32:49');
+INSERT INTO [MESSAGE] (title, is_draft, header, body, background, start_date, end_date) VALUES ('042bis525', 1, null, null, 'ABCDEF', '2021-05-26 18:18:20', '2021-01-09 02:57:42');
+INSERT INTO [MESSAGE] (title, is_draft, header, body, background, start_date, end_date) VALUES ('425vtx604', 1, 'nunc', 'vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae donec pharetra magna', 'ABCDEF', '2021-09-09 02:54:06', '2021-06-10 10:14:04');
+INSERT INTO [MESSAGE] (title, is_draft, header, body, background, start_date, end_date) VALUES ('916ssb119', 1, 'ante', 'platea dictumst aliquam augue quam sollicitudin vitae consectetuer eget rutrum', 'ABCDEF', '2021-01-31 18:54:43', '2020-12-30 20:29:46');
+INSERT INTO [MESSAGE] (title, is_draft, header, body, background, start_date, end_date) VALUES ('287cni236', 1, null, null, null, null, null);
+
+INSERT INTO [MESSAGE_CATEGORY] (message_id, category_id) VALUES (1, 1);
+INSERT INTO [MESSAGE_CATEGORY] (message_id, category_id) VALUES (1, 2);
+INSERT INTO [MESSAGE_CATEGORY] (message_id, category_id) VALUES (1, 3);
+INSERT INTO [MESSAGE_CATEGORY] (message_id, category_id) VALUES (1, 4);
+INSERT INTO [MESSAGE_CATEGORY] (message_id, category_id) VALUES (1, 5);
+INSERT INTO [MESSAGE_CATEGORY] (message_id, category_id) VALUES (1, 6);
+INSERT INTO [MESSAGE_CATEGORY] (message_id, category_id) VALUES (1, 7);
+INSERT INTO [MESSAGE_CATEGORY] (message_id, category_id) VALUES (1, 8);
+INSERT INTO [MESSAGE_CATEGORY] (message_id, category_id) VALUES (1, 9);
+INSERT INTO [MESSAGE_CATEGORY] (message_id, category_id) VALUES (1, 10);
+INSERT INTO [MESSAGE_CATEGORY] (message_id, category_id) VALUES (2, 5);
+INSERT INTO [MESSAGE_CATEGORY] (message_id, category_id) VALUES (3, 4);
+INSERT INTO [MESSAGE_CATEGORY] (message_id, category_id) VALUES (4, 10);
+INSERT INTO [MESSAGE_CATEGORY] (message_id, category_id) VALUES (5, 7);
+INSERT INTO [MESSAGE_CATEGORY] (message_id, category_id) VALUES (6, 5);
+INSERT INTO [MESSAGE_CATEGORY] (message_id, category_id) VALUES (7, 6);
+INSERT INTO [MESSAGE_CATEGORY] (message_id, category_id) VALUES (8, 9);
+INSERT INTO [MESSAGE_CATEGORY] (message_id, category_id) VALUES (9, 9);
+INSERT INTO [MESSAGE_CATEGORY] (message_id, category_id) VALUES (10, 2);
